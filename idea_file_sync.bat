f251c78041cd03d04950c1e28f3823ffc2053c86
hanyangyang <2896306235@qq.com>
2025-03-25 16:53:38 +0800

    idea-file-sync20250325165338

---
+++ idea_file_sync.bat
@@ -0,0 +1,20 @@
+@echo off
+echo "开始执行..."
+
+set "script_dir=%~dp0"
+echo 脚本所在目录是：%script_dir%
+
+%进入脚本所在目录%
+cd /d "%~dp0"
+
+%推送之前先拉取最新的会话%
+git pull
+echo "pull finished"
+%将文件添加到git管理，提交并推送到远程，提交消息为固定内容加当前时间%
+git add .
+set current_time=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m idea-file-sync%current_time%
+echo "commit finished"
+git push
+%结束%
+echo "execute end..."
