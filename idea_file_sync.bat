06b2226a1408daec4d1a66d9744c95a959323c5d
hanyangyang <2896306235@qq.com>
2025-04-22 15:39:08 +0800

    ~0,4-~4,2-~6,2 ~8,2:~10,2:~12,2

--- idea_file_sync.bat
+++ idea_file_sync.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal enabledelayedexpansion
+
 echo "开始执行..."
 
 set "script_dir=%~dp0"
@@ -12,9 +14,30 @@ git pull
 echo "pull finished"
 %将文件添加到git管理，提交并推送到远程，提交消息为固定内容加当前时间%
 git add .
-set current_time=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
-echo '%current_time%'
-git commit -m "%current_time%"
+
+
+
+rem 使用WMIC命令获取标准化的日期时间
+for /f "skip=1 delims=" %%a in ('wmic os get localdatetime ^| findstr /r [0-9]') do (
+    set "datetime=%%a"
+)
+
+rem 提取日期时间各部分
+set "year=%datetime:~0,4%"
+set "month=%datetime:~4,2%"
+set "day=%datetime:~6,2%"
+set "hour=%datetime:~8,2%"
+set "minute=%datetime:~10,2%"
+set "second=%datetime:~12,2%"
+
+rem 组合成指定格式
+set "formatted_time=%year%-%month%-%day% %hour%:%minute%:%second%"
+
+
+
+
+echo '%formatted_time%'
+git commit -m "%formatted_time%"
 echo "commit finished"
 git push
 %结束%
