1453092f9d77f2287ddf9b977b6f310c1ddc2822
hanyangyang <2896306235@qq.com>
2025-04-22 15:22:26 +0800

    commit

--- consoles/.history/db/82650ad6-a50c-432b-b07e-b1b3a18d362c.sql
+++ consoles/.history/db/82650ad6-a50c-432b-b07e-b1b3a18d362c.sql
@@ -3,3 +3,11 @@ INSERT INTO `prescription`.`hospital` (`id`, `hospital_code`, `hospital_name`, `
 SELECT id,hospital_code,hospital_name,mch_id,secret_key,private_key,public_key,pay_public_key,version,day_limit,create_time,creator_id,update_time,updater_id FROM merchant_info WHERE (hospital_code = 009);
 ;-- -. . -..- - / . -. - .-. -.--
 SELECT id,hospital_code,hospital_name,mch_id,secret_key,private_key,public_key,pay_public_key,version,day_limit,create_time,creator_id,update_time,updater_id FROM merchant_info WHERE (hospital_code = '009');
+;-- -. . -..- - / . -. - .-. -.--
+select (@i:=@i+1) as rownum, (select @i:=100) as init;
+;-- -. . -..- - / . -. - .-. -.--
+select (@i:=@i+1) as rownum from dual, (select @i:=100) as init;
+;-- -. . -..- - / . -. - .-. -.--
+select (@i:=@i+1) as rownum from DUAL, (select @i:=100) as init;
+;-- -. . -..- - / . -. - .-. -.--
+alter table hospital add column delivery_config varchar(512) DEFAULT NULL COMMENT '快递配置{"supportList":[],"hintInfo":""}';
--- consoles/db/82650ad6-a50c-432b-b07e-b1b3a18d362c/药事服务测试环境.sql
+++ consoles/db/82650ad6-a50c-432b-b07e-b1b3a18d362c/药事服务测试环境.sql
@@ -1,1 +1,3 @@
 SELECT id,hospital_code,hospital_name,mch_id,secret_key,private_key,public_key,pay_public_key,version,day_limit,create_time,creator_id,update_time,updater_id FROM merchant_info WHERE (hospital_code = '009')
+
+select (@i:=@i+1) as rownum from DUAL, (select @i:=100) as init;
