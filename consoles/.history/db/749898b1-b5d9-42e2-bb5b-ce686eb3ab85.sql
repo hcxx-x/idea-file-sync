800383ed6e2cc3f2b45b5ac82145d2f0e15b423c
hanyangyang <2896306235@qq.com>
2025-05-19 15:59:02 +0800

    idea_file_sync20250519155902

--- consoles/.history/db/749898b1-b5d9-42e2-bb5b-ce686eb3ab85.sql
+++ consoles/.history/db/749898b1-b5d9-42e2-bb5b-ce686eb3ab85.sql
@@ -151,13 +151,9 @@ update prescription_info set commit=0,auto_fill=null,customer_id=null,delivery_t
 ;-- -. . -..- - / . -. - .-. -.--
 select * from prescription_info pi where pi.hospital_code='SZ' order by id desc;
 ;-- -. . -..- - / . -. - .-. -.--
-select * from prescription_info pi where pi.hospital_code like '1001%' order by id desc;
-;-- -. . -..- - / . -. - .-. -.--
 update prescription_info set commit=0,auto_fill=null,customer_id=null,delivery_type=null,delivery_status=null,pay_status=0
                          ,create_time=now()
                          where recipe_code in ('20250429029','20250429027');
-;-- -. . -..- - / . -. - .-. -.--
-select * from hospital;
 ;-- -. . -..- - / . -. - .-. -.--
 select * from prescription_info pi where pi.hospital_code like '009%' order by id desc;
 ;-- -. . -..- - / . -. - .-. -.--
@@ -174,3 +170,23 @@ select * from prescription_info pi where pi.hospital_code like 'SZ%' order by id
 update prescription_info set commit=0,auto_fill=null,customer_id=null,delivery_type=null,delivery_status=null,pay_status=0
                            ,create_time=now()
 where bill_id in ('20240614005');
+;-- -. . -..- - / . -. - .-. -.--
+update prescription_info set commit=0,auto_fill=null,customer_id=null,delivery_type=null,delivery_status=null,pay_status=0
+                           ,create_time='2099-01-01'
+where bill_id in ('20250429020','20250429021');
+;-- -. . -..- - / . -. - .-. -.--
+select * from hospital;
+;-- -. . -..- - / . -. - .-. -.--
+select * from prescription_info pi where pi.hospital_code like '1001%' order by id desc;
+;-- -. . -..- - / . -. - .-. -.--
+update prescription_info set commit=0,auto_fill=null,customer_id=null,delivery_type=null,delivery_status=null,pay_status=0
+                           ,create_time='2099-01-01'
+where bill_id in ('20240614005');
+;-- -. . -..- - / . -. - .-. -.--
+select * from prescription_info where bill_id in ('20240614005');
+;-- -. . -..- - / . -. - .-. -.--
+update prescription_info set commit=0,auto_fill=null,customer_id=null,delivery_type=null,delivery_status=null,pay_status=0
+                         ,create_time='2099-01-01'
+                         where recipe_code in ('20250429029','20250429027');
+;-- -. . -..- - / . -. - .-. -.--
+select * from prescription_info where recipe_code in ('20250429029','20250429027');
--- consoles/db/749898b1-b5d9-42e2-bb5b-ce686eb3ab85/药事服务测试.sql
+++ consoles/db/749898b1-b5d9-42e2-bb5b-ce686eb3ab85/药事服务测试.sql
@@ -2,18 +2,22 @@ select * from hospital;
 -- 1001,1002 省中，sz 市中
 
 
-select * from prescription_info pi where pi.hospital_code like 'SZ%' order by id desc;
+select * from prescription_info pi where pi.hospital_code like '1001%' order by id desc;
 
 -- 重置处方 市中   张测试数据
 update prescription_info set commit=0,auto_fill=null,customer_id=null,delivery_type=null,delivery_status=null,pay_status=0
-                         ,create_time=now()
+                         ,create_time='2099-01-01'
                          where recipe_code in ('20250429029','20250429027');
+
+select * from prescription_info where recipe_code in ('20250429029','20250429027');
 -- 省中  张测试数据
 update prescription_info set commit=0,auto_fill=null,customer_id=null,delivery_type=null,delivery_status=null,pay_status=0
-                           ,create_time=now()
+                           ,create_time='2099-01-01'
 where bill_id in ('20240614005');
+
+select * from prescription_info where bill_id in ('20240614005');
 
 -- 妇幼  李测试数据
 update prescription_info set commit=0,auto_fill=null,customer_id=null,delivery_type=null,delivery_status=null,pay_status=0
-                           ,create_time=now()
+                           ,create_time='2099-01-01'
 where bill_id in ('20250429020','20250429021')
