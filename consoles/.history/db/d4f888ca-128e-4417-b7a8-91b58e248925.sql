a97de19e21a6d4a912524c408f9a9494697e6db8
hanyangyang <2896306235@qq.com>
2025-05-21 16:59:02 +0800

    idea_file_sync20250521165902

--- consoles/.history/db/d4f888ca-128e-4417-b7a8-91b58e248925.sql
+++ consoles/.history/db/d4f888ca-128e-4417-b7a8-91b58e248925.sql
@@ -3,3 +3,17 @@ alter table trade_role_setting add column status varchar(16) default null commen
 select * from d16_t_gl_balances where com_code='01' and inter_code in ('02','06');
 ;-- -. . -..- - / . -. - .-. -.--
 select * from d16_t_gl_balances where com_code='01' and inter_code in ('02','06') limit 1;
+;-- -. . -..- - / . -. - .-. -.--
+select trs.id,trs.rule_code,trs.biz_entity_code,bbe.biz_entity_name bizEntityName,bbe.vat_entity_name vatEntityName,
+        trs.opp_entity_code,trs.trade_subject,trs.trade_role,trs.status,trs.review_status,trs.start_time,trs.end_time,
+        trs.maintain_name
+        from trade_role_setting trs left join base_biz_entity bbe on trs.biz_entity_code = bbe.biz_entity_code;
+;-- -. . -..- - / . -. - .-. -.--
+select trs.id,bbe.id,trs.rule_code,trs.biz_entity_code,bbe.biz_entity_name bizEntityName,bbe.vat_entity_name vatEntityName,
+        trs.opp_entity_code,trs.trade_subject,trs.trade_role,trs.status,trs.review_status,trs.start_time,trs.end_time,
+        trs.maintain_name;
+;-- -. . -..- - / . -. - .-. -.--
+select trs.id,bbe.id,trs.rule_code,trs.biz_entity_code,bbe.biz_entity_name bizEntityName,bbe.vat_entity_name vatEntityName,
+        trs.opp_entity_code,trs.trade_subject,trs.trade_role,trs.status,trs.review_status,trs.start_time,trs.end_time,
+        trs.maintain_name
+        from trade_role_setting trs left join base_biz_entity bbe on trs.biz_entity_code = bbe.biz_entity_code;
